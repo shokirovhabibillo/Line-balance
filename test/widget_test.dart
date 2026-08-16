e5e3930880c0f878f1546b9e4ed663144020c1b0
shokirovhabibillo <shokirovhabibillo@gmail.com>
2026-08-16 22:43:05 +0500

    Refactor timing in Time Study test
    Updated the test to use runAsync for cycle timing.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:line_balance_platform/app/app.dart';
 
 void main() {
   testWidgets('home page renders', (tester) async {
     await tester.pumpWidget(const LineBalanceApp());
+
     expect(find.text('Line Balance Platform'), findsOneWidget);
     expect(find.text('Time Study'), findsOneWidget);
   });
 
   testWidgets('Time Study records one cycle', (tester) async {
     await tester.pumpWidget(const LineBalanceApp());
+
     await tester.tap(find.text('Time Study'));
     await tester.pumpAndSettle();
 
     expect(find.text('Cycle yozuvlari (0)'), findsOneWidget);
+
     await tester.tap(find.text('Start cycle'));
-    await tester.pump(const Duration(milliseconds: 120));
+
+    await tester.runAsync(() async {
+      await Future<void>.delayed(const Duration(milliseconds: 150));
+    });
+
     await tester.tap(find.text('Finish cycle'));
     await tester.pump();
 
